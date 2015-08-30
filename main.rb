8f40308d1c82f6a351c82258ad5d31d32c678aa1
swillett <swillett@outlook.com>
2015-08-30 21:07:40 +0000

    Add threading to main. Refresh config file and main ping logic

--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
 require './network_utils.rb'
 include NetworkUtils
 
+semaphore = Mutex.new
+
 config = {
     polling_interval: 2,
+    refresh_interval: 15,
     dns_timeout: 1,
     ping_timeout: 1,
 }
@@ -12,11 +15,22 @@ hosts = [
     { id: '2', type: 'ICMP', node: 'test.com', port: '80', status: 'offline' }
 ]
 
+timer = Thread.new { 
+    while true
+        sleep config[:refresh_interval]
+        semaphore.synchronize {
+            puts "hi"
+        }
+    end
+}
+
 NetworkUtils.resolve_nodes(hosts)
 
 while true
-    NetworkUtils.ping_nodes(hosts)
-    NetworkUtils.resolve_nodes( hosts.select { |host| host[:status] == 'dns_error'  } )
-    puts hosts
     sleep config[:polling_interval]
+    semaphore.synchronize {
+        NetworkUtils.ping_nodes(hosts)
+        NetworkUtils.resolve_nodes( hosts.select { |host| host[:status] == 'dns_error'  } )
+        puts hosts
+    }
 end
